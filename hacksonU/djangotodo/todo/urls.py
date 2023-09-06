60dc2a8c21667dd3ce1bea69e426d023748fa804
tarakokko3233 <tanahashist1230@outlook.jp>
2023-09-06 18:17:51 +0900

    a

--- hacksonU/djangotodo/todo/urls.py
+++ hacksonU/djangotodo/todo/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.TodoList.as_view(), name="list"),
-    path("detail/", views.TodoDetail.as_view(), name="detail"),
+    path('detail/<int:pk>/', views.TodoDetail.as_view(), name='detail') ,
     path('todo_home/', views.TaskListView.as_view(), name='todo_home'),
 ]
