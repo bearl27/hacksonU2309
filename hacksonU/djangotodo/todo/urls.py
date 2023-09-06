385924837ce50402b02f568ed201758b3e4227dc
tarakokko3233 <tanahashist1230@outlook.jp>
2023-09-06 16:25:34 +0900

    z

--- hacksonU/djangotodo/todo/urls.py
+++ hacksonU/djangotodo/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TodoDetail, TodoList
+from .views import TodoDetail, TodoList, ToDoHome
 
 urlpatterns = [
     path("", TodoList.as_view(), name="list"),
     path("detail/<int:pk>", TodoDetail.as_view(), name="detail"),
+    path('', ToDoHome.as_view(), name='home')
 ]
--- hacksonU/djangotodo/todo/views.py
+++ hacksonU/djangotodo/todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import Todo
 
 
@@ -10,3 +10,7 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     model = Todo
     context_object_name = "task"
+
+class ToDoHome(TemplateView):
+    template_name = "todo_home.html"
+    context_object_name = "home"
