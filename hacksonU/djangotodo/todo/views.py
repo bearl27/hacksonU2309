4f260c6323d9f3c889743930107cdbe3b54857a2
zono0013 <143684883+zono0013@users.noreply.github.com>
2023-09-08 09:04:50 +0900

    a

--- hacksonU/djangotodo/todo/views.py
+++ hacksonU/djangotodo/todo/views.py
@@ -8,26 +8,37 @@ class TodoList(ListView):
     model = Todo
     context_object_name = "tasks"
 
+
 class TodoDetail(DetailView):
     model = Todo
     context_object_name = "task"
+
 
 class TaskListView(ListView):
     model = Todo
     template_name = 'todo/todo_home.html'
     context_object_name = "tasks"
 
+
 class TodoCreate(CreateView):
     model = Todo
     fields = "__all__"
     success_url = reverse_lazy("list")
+
 
 class TodoUpdate(UpdateView):
     model = Todo
     fields = "__all__"
     success_url = reverse_lazy("list")
 
+
 class TodoDelete(DeleteView):
     model = Todo
     context_object_name = "task"
     success_url = reverse_lazy("list")
+
+
+class TodoCalender(ListView):
+    model = Todo
+    template_name = 'todo/todo_calender.html'
+    context_object_name = "tasks"
