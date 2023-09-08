811d5e13280276ac60f778c9a063032f2319e005
tarakokko3233 <tanahashist1230@outlook.jp>
2023-09-08 13:08:04 +0900

    a

--- hacksonU/djangotodo/todo/models.py
+++ hacksonU/djangotodo/todo/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Todo(models.Model):
     title = models.CharField("タスク名", max_length=30)
     description = models.TextField("詳細", blank=True)
-    deadline = models.DateField("締切",null = True)
+    deadline = models.DateField("締切",null = True, blank = True)
     importance = models.IntegerField("重みづけ",validators=[MinValueValidator(1), MaxValueValidator(5)],null = True)
 
     def __str__(self):
