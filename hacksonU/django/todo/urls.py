39ba2b5c89e56ea7825235cbfda6307ec28462b9
tarakokko3233 <tanahashist1230@outlook.jp>
2023-09-10 14:57:51 +0900

    直った

---
+++ hacksonU/django/todo/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import TodoDetail,TaskListView, TodoUpdate, TodoDelete, TodoCalender, TodoCategory
+from . import views
+
+urlpatterns = [
+    path("", views.todo_list, name="list"),
+    path('detail/<int:pk>/', TodoDetail.as_view(), name='detail'),
+    path('todo_home/', TaskListView.as_view(), name='todo_home'),
+    path('update/<int:pk>/', TodoUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', TodoDelete.as_view(), name='delete'),
+    path('todo_category/', TodoCategory.as_view(), name='category'),
+    path('todo_category/', TodoCategory.as_view(), name='category'),
+    path('todo_calender/<int:year>/<int:month>/',TodoCalender.as_view(), name='calender'),
+    path('create/', views.create_todo, name='create_todo'),
+]
